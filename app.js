36075b8aa244a9e5cc27a0b5119f7b13821d313e
stellarutg <stellarut95@gmail.com>
2018-01-21 11:45:07 +0000

    Delay between emails etc.

--- app.js
+++ app.js
@@ -42,25 +42,47 @@ let createEmail = (mailInfo) => {
 
   // Email body
   let emailtext = "";
-  emailtext += 'Daginn,' + enter() + enter(); // Tvö enter fyrir space
-  emailtext += 'Við erum hérna frá Nörd að gera drasl' + enter();
-  emailtext += `Hafið þið, ${Name}, áhuga á að auglýsa?` + enter() + enter();
-  emailtext += 'Kveðja, Ketill og vinir';
+  emailtext += 'Góðan daginn,' + enter() + enter();
+
+  emailtext += 'Stella Rut heiti ég og hef samband við þig fyrir hönd Nörd, nemendafélags tölvunarfræði- og hugbúnaðarverfrkæðinema við Háskóla Íslands.' + enter() + enter();
+
+  emailtext += 'Útskriftarhópur tölvunarfræði- og hugbúnaðarverkfræði stendur fyrir útgáfu tímaritsins Kóðinn sem gefinn er út til styrktar útskriftar- og námsferð verkfræðinema. Um er að ræða árlegt tímarit sem inniheldur umfjallanir um kennara, starfsemi deildarinnar, útskrifaða nemendur og afrek og verkefni núverandi nema við Iðnaðarverkfræði-, vélaverkfræði- og tölvunarfræðideild Háskóla Íslands. Kóðanum er dreift í allar 25 byggingar Háskóla Íslands og verður 15.000 nemendum og starfsmönnum aðgengilegt ásamt dreifingu á greinum á veraldarvefnum allan ársins hring. Ásamt því verður blaðið sent út til nýlegra útskrifaðra tölvunarfræði- og hugbúnaðarverkfræðinema.' + enter() + enter();
+
+  emailtext += 'Eftirfarandi auglýsingar eru í boði:' + enter();
+  emailtext += '- Baksíða kr. 180.000,-' + enter();
+  emailtext += '- Heilsíða kr. 125.000,-' + enter();
+  emailtext += '- 1/2 síða kr. 75.000,-' + enter();
+  emailtext += '- 1/4 síða kr. 40.000,-' + enter();
+  emailtext += '- Logo kr. 15.000,-' + enter() + enter();
+
+  emailtext += 'Mér þætti vænt um ef þið sæjuð ykkur fært að styrkja nemendurna með auglýsingu í blaðinu, en útgáfa Kóðans er einn stærsti fjáröflunarþáttur ferðarinnar.' + enter() + enter();
+
+
+  emailtext += 'Með von um samstarf,' + enter();
+  emailtext += 'Stella Rut' + enter();
+  emailtext += 'Gjaldkeri Nörd' + enter();
+  emailtext += 'F.h. útskriftarnefndar' + enter();
 
   // Sends the email 
-  if(Mail) sendMailTo(emailtext, Name, Mail);
+  
+  const randomDelay = Math.floor(Math.random() * 800) + 100;
+  setTimeout(() => {
+    if(Mail) sendMailTo(emailtext, Name, Mail, 0);
+  }, randomDelay);
 };
 
 // Simple way to add enter to a text string
 let enter = () => " "+ "\r\n";
 
+let globalMailCounter = 0;
+
 // Sends the mail
-let sendMailTo = (emailtext, Name, Mail) => {
+let sendMailTo = (emailtext, Name, Mail, errorCount) => {
   // Options
   var mailOptions = {
     from: authData[2] ? authData[2] : authData[0] + '@gmail.com',
     to: Mail,
-    subject: 'Styrkja mig????',
+    subject: 'Kóðinn 2018 - auglýsing',
     text: emailtext,
   };
   // Send the mail
@@ -70,9 +92,22 @@ let sendMailTo = (emailtext, Name, Mail) => {
       console.log("error on: " + Mail);
       console.log(error);
       console.log("Retrying...");
-      sendMailTo(emailtext, Name, Mail) 
+      if(errorCount > 7) {
+        console.log(`Failed to send mail to ${Mail} 7 times, skipping`);
+        return;
+      } else {
+        setTimeout(() => {
+          sendMailTo(emailtext, Name, Mail, errorCount + 1);
+        }, 2000);
+      }
     } else {
-      console.log('Email sent to: ' + Mail + ' ' + info.response);
+      const outString = globalMailCounter + '\t Email sent to: ' + Mail + ' ' + info.response;
+      console.log(outString);
+      globalMailCounter++;
+      fs.appendFile('out.txt',outString + '\n',() => {
+
+      })
+
     }
   });
 }
